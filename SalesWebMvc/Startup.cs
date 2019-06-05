6d671cbbf31312800bab122bf9a93bc62c59f367
renanosoriorosa <renanosoriogd@gmail.com>
2019-06-05 20:58:12 -0300

    Service e findall de sellers

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -41,6 +42,7 @@ namespace SalesWebMvc
                     builder.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>(); // registra o serviço
+            services.AddScoped<SellerService>(); // registra o serviço
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
