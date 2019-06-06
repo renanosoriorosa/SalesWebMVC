533205a57a231216895e55876c23ff09b023633c
renanosoriorosa <renanosoriogd@gmail.com>
2019-06-06 20:49:17 -0300

    add o insert de sellers com select de department

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -43,6 +43,7 @@ namespace SalesWebMvc
 
             services.AddScoped<SeedingService>(); // registra o serviço
             services.AddScoped<SellerService>(); // registra o serviço
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
