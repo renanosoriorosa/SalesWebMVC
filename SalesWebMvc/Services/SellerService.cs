40bd873320922afb6d783ded57a35f50e0014887
renanosoriorosa <renanosoriogd@gmail.com>
2019-06-09 16:41:47 -0300

    tratamento de exception ao deletar seller vinculado a uma venda

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -53,9 +53,18 @@ namespace SalesWebMvc.Services
 
         public async Task  RemoveAsync( int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+
+                throw new IntegrityException("Não foi possivel deletar o vendedor por que ele/ela tem vendas");
+            }
+            
         }
 
         public async Task UpdateAsync(Seller obj)
