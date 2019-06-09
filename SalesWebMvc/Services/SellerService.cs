e5c4f8c46c88cfffd0fb22690b5cceb74327c671
renanosoriorosa <renanosoriogd@gmail.com>
2019-06-09 18:52:44 -0300

    implementei grounpingsearch

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -59,7 +59,7 @@ namespace SalesWebMvc.Services
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateException e)
+            catch (DbUpdateException)
             {
 
                 throw new IntegrityException("Não foi possivel deletar o vendedor por que ele/ela tem vendas");
